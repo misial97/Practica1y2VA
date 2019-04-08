097af69996b7a8115b6879cc0dcc99280a3291d9
misial <miguelsierra-11@hotmail.com>
2019-04-08 11:59:48 +0200

    encapsulacion main

--- mainPractica.py
+++ mainPractica.py
@@ -7,15 +7,15 @@ import argparse
 _cte_relacionMaxAnchoAlto = 1.5
 _cte_relacionMinAnchoAlto = 0.5
 _colorVerdeCaja = (0, 255, 0)
-mascaraMediaPrecaucion = cv2.imread("/home/misial/PycharmProjects/practica2/mascaras/mascaraMediaPrecaucion.ppm")
-mascaraMediaProhibicion = cv2.imread("/home/misial/PycharmProjects/practica2/mascaras/mascaraMediaProhibicion.ppm")
-mascaraMediaSTOP = cv2.imread("/home/misial/PycharmProjects/practica2/mascaras/mascaraMediaSTOP.ppm")
+mascaraMediaPrecaucion = cv2.imread("./mascaras/mascaraMediaPrecaucion.ppm")
+mascaraMediaProhibicion = cv2.imread("./mascaras/mascaraMediaProhibicion.ppm")
+mascaraMediaSTOP = cv2.imread("./mascaras/mascaraMediaSTOP.ppm")
 
 mascaraMediaProhibicion = cv2.cvtColor(mascaraMediaProhibicion, cv2.COLOR_BGR2GRAY)
 mascaraMediaPrecaucion = cv2.cvtColor(mascaraMediaPrecaucion, cv2.COLOR_BGR2GRAY)
 mascaraMediaSTOP = cv2.cvtColor(mascaraMediaSTOP, cv2.COLOR_BGR2GRAY)
 # constructor mser
-_delta = 7
+_delta = 10
 _min_area = 100
 _max_area = 2000
 _max_variation = 0.2
@@ -24,41 +24,84 @@ _max_evolution = 200
 _area_threshold = 1.01
 _min_margin = 0.003
 _edge_blur_size = 5
+rects = []
+# Rojos:
+rojo_bajos1 = np.array([0, 50, 50], dtype=np.uint8)
+rojo_bajos2 = np.array([240, 50, 50], dtype=np.uint8)
+rojo_altos1 = np.array([12, 255, 255], dtype=np.uint8)
+rojo_altos2 = np.array([256, 255, 255], dtype=np.uint8)
 
-cv2.imshow("mascaramediaconstante", mascaraMediaPrecaucion)
-cv2.waitKey()
 listaDir = os.listdir("/home/misial/Descargas/train/")
 listaDir.sort()
-print(listaDir[0].title())
-for archivo in listaDir:
 
-    # cargamos imagen
-    img = cv2.imread("/home/misial/Descargas/train/" + archivo.title().lower())
-    # cv2.imshow("original", img)
-    vis = img.copy()
-    # cv2.waitKey()
+def crea_compara_mascaras():
+    for i in range(0, len(rects)):
+        #print(str(i + 1) + "pinta mascaras")
+        (x, y, w, h) = rects[i]
+        if (x > 0) and (y > 0) and (y + h > 0):
+            crop_img = img[y:y + h, x:x + w]
+            # No sabemos cómo se hace para ajustar la imagen a 25 píxeles
+            # print(str(x)+","+str(y)+","+str(w)+","+str(h))
+            img_recortada = cv2.resize(crop_img, (25, 25), interpolation=cv2.INTER_NEAREST)
+            hsv = cv2.cvtColor(img_recortada, cv2.COLOR_BGR2HSV)
 
-    # convertimos a escala de grises
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("escala_grises",gray)
-    # cv2.waitKey()
+            # Crear las mascaras
+            mascara_rojo1 = cv2.inRange(hsv, rojo_bajos1, rojo_altos1)
+            mascara_rojo2 = cv2.inRange(hsv, rojo_bajos2, rojo_altos2)
 
-    # equalizamos histograma para mejorar contraste (detecta más señales usando esta imagen que la escala grises normal)
-    grayEqHist = cv2.equalizeHist(gray)
-    # cv2.imshow("hist", grayEqHist)
-    # cv2.waitKey()
+            # Juntar todas las mascaras
+            mask = cv2.add(mascara_rojo1, mascara_rojo2)
 
-    # detectamos regiones alto contraste
+            # comparacion de mascaras
+            aux_mask_pro = (mask * mascaraMediaProhibicion)
+            aux_mask_pre = (mask * mascaraMediaPrecaucion)
+            aux_mask_stp = (mask * mascaraMediaSTOP)
+
+            pix_mask_pro = np.sum(aux_mask_pro)
+            pix_mask_pre = np.sum(aux_mask_pre)
+            pix_mask_stp = np.sum(aux_mask_stp)
+
+            # print("Pro: " + str(pix_mask_pro))
+            # print("Pre: " + str(pix_mask_pre))
+            # print("Stop: " + str(pix_mask_stp))
+            if (pix_mask_pre > 150) and (pix_mask_pro > 150) and (pix_mask_stp > 150):
+                if (pix_mask_pro > pix_mask_pre) and (pix_mask_pro > pix_mask_stp):
+                    cv2.imshow("PROHIBICION", mask)
+                    cv2.waitKey()
+                elif (pix_mask_pre > pix_mask_pro) and (pix_mask_pre > pix_mask_stp):
+                    cv2.imshow("PRECAUCION", mask)
+                    cv2.waitKey()
+                elif (pix_mask_stp > pix_mask_pre) and (pix_mask_stp > pix_mask_pro):
+                    cv2.imshow("STOP", mask)
+                    cv2.waitKey()
+
+            '''
+                        if (pix_mask_pre > 150) and (pix_mask_pro > 10) and (pix_mask_stp > 10):
+                nombre = "Mascara" + str(i)
+                cv2.imshow(nombre, mask)
+                cv2.waitKey()
+            if (pix_mask_pre > 100) and (pix_mask_pro > 100) and (pix_mask_stp > 100):
+                if (pix_mask_pro > pix_mask_pre) and (pix_mask_pro > pix_mask_stp):
+                    cv2.imshow("PROHIBICION", mask)
+                    cv2.waitKey()
+                elif (pix_mask_pre > pix_mask_pro) and (pix_mask_pre > pix_mask_stp):
+                    cv2.imshow("PRECAUCION", mask)
+                    cv2.waitKey()
+                elif (pix_mask_stp > pix_mask_pre) and (pix_mask_stp > pix_mask_pro):
+                    cv2.imshow("STOP", mask)
+                    cv2.waitKey()
+            '''
+
+
+def regiones_detectadas():
+
     mser = cv2.MSER_create(_delta, _min_area, _max_area, _max_variation, _min_diversity, _max_evolution,
                            _area_threshold, _min_margin, _edge_blur_size)
     regions, _ = mser.detectRegions(grayEqHist)
     hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
-    filtrado_rects = []
-    rects = []
-
     # pintamos los rectangulos de las regiones detectadas
-    for i in range(0, len(regions)):
-        x, y, w, h = cv2.boundingRect(regions[i])
+    for j in range(0, len(regions)):
+        x, y, w, h = cv2.boundingRect(regions[j])
         relacion = w / h
         if (relacion < _cte_relacionMaxAnchoAlto) and (relacion > _cte_relacionMinAnchoAlto):
             # print(len(filtrado_rects))
@@ -68,45 +111,48 @@ for archivo in listaDir:
             rects.append(rect2)
             cv2.rectangle(vis, (x - (w // 10), y - (w // 10)), (x + w + (w // 10), y + h + (w // 10)), _colorVerdeCaja)
             cv2.rectangle(vis, (x - (w // 4), y - (w // 4)), (x + w + (w // 4), y + h + (w // 4)), _colorVerdeCaja)
-    print(archivo.title())
-    cv2.imshow('detecciones', vis)
-    cv2.waitKey()
 
-    # habria que continuar por "2 Utilizar el espacio de color HSV para localizar los píxeles que sean de color rojo"
-    # Rojos:
-    rojo_bajos1 = np.array([0, 50, 50], dtype=np.uint8)
-    rojo_bajos2 = np.array([240, 50, 50], dtype=np.uint8)
-    rojo_altos1 = np.array([12, 255, 255], dtype=np.uint8)
-    rojo_altos2 = np.array([256, 255, 255], dtype=np.uint8)
 
-    for i in range(0, len(rects)):
-        (x, y, w, h) = rects[i]
-        crop_img = img[y:y + h, x:x + w]
-        # No sabemos cómo se hace para ajustar la imagen a 25 píxeles
-        img_recortada = cv2.resize(crop_img, (25, 25), interpolation=cv2.INTER_NEAREST)
-        hsv = cv2.cvtColor(img_recortada, cv2.COLOR_BGR2HSV)
+def main():
+    global img, vis, grayEqHist
+    for archivo in listaDir:
+        # print(str(num) + "archivo")
+        # cargamos imagen
+        img = cv2.imread("/home/misial/Descargas/train/" + archivo.title().lower())
 
-        # Crear las mascaras
-        mascara_rojo1 = cv2.inRange(hsv, rojo_bajos1, rojo_altos1)
-        mascara_rojo2 = cv2.inRange(hsv, rojo_bajos2, rojo_altos2)
+        # cv2.imshow("original", img)
+        vis = img.copy()
+        # cv2.waitKey()
 
-        # Juntar todas las mascaras
-        mask = cv2.add(mascara_rojo1, mascara_rojo2)
+        # convertimos a escala de grises
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow("escala_grises",gray)
+        # cv2.waitKey()
 
-        #comparacion de mascaras
+        # equalizamos histograma para mejorar contraste (detecta más señales usando esta imagen que la escala grises normal)
+        grayEqHist = cv2.equalizeHist(gray)
+        # cv2.imshow("hist", grayEqHist)
+        # cv2.waitKey()
 
-        aux_mask_pro = (mask * mascaraMediaProhibicion)
-        print("Pro: " + str(np.sum(aux_mask_pro)))
-        aux_mask_pre = (mask * mascaraMediaPrecaucion)
-        print("Pre: " + str(np.sum(aux_mask_pre)))
-        aux_mask_stp = (mask * mascaraMediaSTOP)
-        print("Stop: " + str(np.sum(aux_mask_stp)))
+        # detectamos regiones alto contraste
+        regiones_detectadas()
 
-    # comparar mascara con mascara media (no sabemos si asi puesta la media funciona)
-    # nombre = "Mascara" + str(i)
-    # cv2.imshow(nombre, mask)
-    # cv2.waitKey()
-    '''
+        print(archivo.title())
+        cv2.imshow('detecciones', vis)
+        cv2.waitKey()
+        # print("longitud rects=" + str(len(rects)))
+
+        crea_compara_mascaras()
+
+
+main()
+
+
+# comparar mascara con mascara media (no sabemos si asi puesta la media funciona)
+# nombre = "Mascara" + str(i)
+#    cv2.imshow("mascara", mask)
+#   cv2.waitKey()
+'''
                 # Eliminar duplicidades
             if len(filtrado_rects) == 0:
                 rect = (x, y, w, h)
@@ -129,4 +175,4 @@ for archivo in listaDir:
                     rect = (x-(w//10), y-(w//10), w+(w//10), h+(w//10))
                     rects.append(rect)
                     cv2.rectangle(vis, (x-(w//10), y-(w//10)), (x + w+(w//10), y + h+(w//10)), _colorVerdeCaja)
-    '''
+'''
