ed168103d23b9bd31d81dcd135ce464a14a7bce1
misial <miguelsierra-11@hotmail.com>
2019-05-11 13:09:31 +0200

    LDA y reconocimiento hecho
    Funciona ok

--- Practica2VA/main2.py
+++ Practica2VA/main2.py
@@ -4,6 +4,7 @@ import os
 import cv2
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
 # Parametros HOGDescriptor
 _winSize = (30, 30)
 _blockSize = (15, 15)
@@ -14,27 +15,32 @@ _nbins = 9
 _numeroImagenes = 846
 _tamanyoHOG = 1296
 
+
 def main():
     # Captacion de argumentos consola
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--train_path", "--train_path", help="Ruta carpeta imagenes de entrenamiento")
     parser.add_argument("--test_path", "--test_path", help="Ruta carpeta imagenes test")
-    parser.add_argument("--detector", "--detector", help="Detector deseado")
+    parser.add_argument("--detector", "--detector", help="Detector deseado (LDA-BAYES)")
 
     args = parser.parse_args()
 
-    entrenamiento = args.train_path
     test = args.test_path
     train = args.train_path
+    detector = args.detector
+
     # Cargado de todas las imagenes de la ruta a testear
     lista_dir = os.listdir(train)
     lista_dir.sort()
 
     matrizCarac = np.zeros((1, 1296))
-    matrizEtiq = np.zeros((_numeroImagenes, 1))
+    matrizCaracTest = np.zeros((1, 1296))
+    etiquetasImg = np.zeros(_numeroImagenes)
     contadorImagenes = 0
+    contadorImagenesTest = 0
 
+    # Entrenamiento del clasificador
     for carpeta in lista_dir:
         ruta_actual = os.path.join(train, carpeta)
         lista_img = os.listdir(ruta_actual)
@@ -61,26 +67,68 @@ def main():
             hog = cv2.HOGDescriptor(_winSize, _blockSize, _blockStride, _cellSize, _nbins)
             calculoHOG = hog.compute(gray_eq)
 
-            #Insertamos DescripcionHOG en la matriz
-            #print(np.transpose(calculoHOG).shape)
-
+            # Insertamos DescripcionHOG en la matriz
+            # print(np.transpose(calculoHOG).shape)
             # si es la primera fila, sustituye la inicializada a 0
             if contadorImagenes == 0:
                 matrizCarac[0] = np.transpose(calculoHOG)
             else:
                 matrizCarac = np.concatenate((matrizCarac, np.transpose(calculoHOG)))
-            matrizEtiq[contadorImagenes, 0] = str(carpeta.title())
+            etiquetasImg[contadorImagenes] = str(carpeta.title())
             contadorImagenes = contadorImagenes + 1
-            #print(calculoHOG)
-            #print(len(calculoHOG))
+            # print(calculoHOG)
+            # print(len(calculoHOG))
 
-    print("Numero imagenes train totales =" + str(contadorImagenes))
-    print(matrizCarac)
-    print(matrizCarac.shape)
-    print(".----------------------------")
-    print(matrizEtiq)
-    print(matrizEtiq.shape)
+    # Imagenes test
+    ruta_actual_test = os.path.join(test)
+    print(ruta_actual_test)
+    lista_img_test = os.listdir(test)
+    lista_img_test.sort()
+    for archivo in lista_img_test:
+        if not(archivo.title()[0] == "."):
+            titulo = archivo.title().lower()
+            print(titulo)
+            # print(titulo)
+            # Cargamos imagen
+            img = cv2.imread(os.path.join(ruta_actual_test, titulo))
 
+            # Redimensionamos la imagen a 30x30
+            img_recortada = cv2.resize(img, (30, 30), interpolation=cv2.INTER_LINEAR)
+
+            # Convertimos a escala de grises
+            gray = cv2.cvtColor(img_recortada, cv2.COLOR_BGR2GRAY)
+
+            # Equalizamos hist. para mejorar contraste (detecta mas senyales usando esta imagen que la escala grises normal)
+            gray_eq = cv2.equalizeHist(gray)
+
+            # Calculamos HOG
+            hog = cv2.HOGDescriptor(_winSize, _blockSize, _blockStride, _cellSize, _nbins)
+            calculoHOG = hog.compute(gray_eq)
+
+            # Insertamos DescripcionHOG en la matriz
+            # print(np.transpose(calculoHOG).shape)
+            # si es la primera fila, sustituye la inicializada a 0
+            if contadorImagenesTest == 0:
+                matrizCaracTest[0] = np.transpose(calculoHOG)
+            else:
+                matrizCaracTest = np.concatenate((matrizCaracTest, np.transpose(calculoHOG)))
+            contadorImagenesTest = contadorImagenesTest + 1
+
+    # Ya con las matrices de los descrp y de las etiquetas creadas --> LDA(reduc. dimensionalidad)
+    lda = LinearDiscriminantAnalysis()
+    lda.fit(matrizCarac, etiquetasImg)
+
+    calculoLDA = lda.transform(matrizCarac)
+    print(lda.predict(matrizCaracTest))
+
+    '''
+        print("Numero imagenes train totales =" + str(contadorImagenes))
+        print(matrizCarac)
+        print(matrizCarac.shape)
+        print(".----------------------------")
+        print(etiquetasImg)
+        print(etiquetasImg.shape)
+    '''
 
 main()
 print("finalizado")
@@ -94,10 +142,7 @@ y_a = np.zeros(648,3)
 y_b = np.ones(648,3)
 y = np.concatenate((y_a, y_b))
 
-lda = LinearDiscriminantAnalysis()
-lda.fit(X, y)
 
-calculoLDA = lda.transform(X)
 
 print("-----------------------")
 print(calculoLDA)
